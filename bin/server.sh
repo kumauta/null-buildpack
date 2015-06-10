b213419bb918794055954361610edeb8f18062d8
kumauta <nogami.s@gmail.com>
2015-06-10 14:31:15 +0900

    Add resin

--- bin/server.sh
+++ bin/server.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 echo "-------------------------------------------------"
-curl -s http://ip.kumauta.com
+echo "Hosting dea : `curl -s http://ip.kumauta.com`"
 echo ""
 echo "-------------------------------------------------"
-echo ${PORT}
+echo "Listen port : ${PORT}"
 echo "-------------------------------------------------"
 pwd
 ./jre/bin/java -version
 echo "-------------------------------------------------"
+export JAVA_HOME=/home/vcap/app/jre/bin
+export PATH=/home/vcap/app/jre/bin:${PATH}
+java -version
+echo "-------------------------------------------------"
+
 exec perl http2.pl $PORT
